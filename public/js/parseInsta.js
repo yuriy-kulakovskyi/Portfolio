61e797278586d627c880fe147605888150dc7e4c
Yuriy Kulakovskyi <84936189+yuriy-kulakovskyi@users.noreply.github.com>
2022-06-06 17:49:46 +0300

    I have deleted not important code

--- public/js/parseInsta.js
+++ public/js/parseInsta.js
@@ -8,11 +8,3 @@ InstaClient.authBySessionId('11634522357%3A8y6xKnqG2iAh9R%3A7')
 
 		fs.writeFileSync('parseInstainfo.js', '$("#instagram__username").text("'+ profile.first_name +'"); $("#instagram__nickname").text("'+profile.username+'"); $("#instagram__bio").text(`'+ profile.biography +'`)');
 	})
-
-InstaClient.authBySessionId('11634522357%3A8y6xKnqG2iAh9R%3A7')
-	.then(profile => {
-    console.log(profile);
-
-		fs.writeFileSync('parseInstainfo.js', '$("#instagram__username").text("'+ profile.first_name +'"); $("#instagram__nickname").text("'+profile.username+'"); $("#instagram__bio").text(`'+ profile.biography +'`)');
-	})
-	.catch(err => console.error(err));
