9adb82ae57975ba897b5ae8c268b9e3a45c1f246
Yuriy Kulakovskyi <84936189+yuriy-kulakovskyi@users.noreply.github.com>
2022-06-06 17:50:35 +0300

    Update parseInsta.js

--- public/js/parseInsta.js
+++ public/js/parseInsta.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 
 InstaClient.authBySessionId('11634522357%3A8y6xKnqG2iAh9R%3A7')
 	.then(profile => {
-    console.log(profile);
 
 		fs.writeFileSync('parseInstainfo.js', '$("#instagram__username").text("'+ profile.first_name +'"); $("#instagram__nickname").text("'+profile.username+'"); $("#instagram__bio").text(`'+ profile.biography +'`)');
 	})
