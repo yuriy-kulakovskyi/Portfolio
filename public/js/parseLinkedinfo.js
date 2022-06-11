3725a8834c5290d7f5b24a2a8310a717f154d6c5
Yuriy Kulakovskyi <84936189+yuriy-kulakovskyi@users.noreply.github.com>
2022-06-11 13:38:59 +0300

    updated linkedin avatar

--- public/js/parseLinkedinfo.js
+++ public/js/parseLinkedinfo.js
@@ -1,4 +1,4 @@
-$("#linkedin__image").css("background-image", "url(https://media-exp1.licdn.com/dms/image/C4E03AQGrotURUDkYUQ/profile-displayphoto-shrink_200_200/0/1651232984487?e=1657152000&v=beta&t=aNITvWXdYqaKnHdBKi4SmSr-BXxZnDWg51cHn4m5e4Y)");
+$("#linkedin__image").css("background-image", "url(https://media-exp2.licdn.com/dms/image/C4E03AQFwwCBylvg5Kg/profile-displayphoto-shrink_400_400/0/1654943253050?e=1660176000&v=beta&t=5IuvYiGElHg9bPZbJJp5WqIssOm7Gnp87Qq3cqaraqo");
 $("#instagram__image").css("background-image", "url(https://media-exp1.licdn.com/dms/image/C4E03AQGrotURUDkYUQ/profile-displayphoto-shrink_200_200/0/1651232984487?e=1657152000&v=beta&t=aNITvWXdYqaKnHdBKi4SmSr-BXxZnDWg51cHn4m5e4Y)");
 $("#nickname").text('yuriy-kulakovskyi');
 $("#username").text('Yuriy Kulakovskyi');
