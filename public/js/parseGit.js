1d329099c6cee742bf197cbb540942e9ff3a18a7
yuriy-kulakovskyi <yurakulak42@gmail.com>
2023-01-26 13:23:25 +0200

    Projects update

--- public/js/parseGit.js
+++ public/js/parseGit.js
@@ -1,44 +1,67 @@
-fetch('https://api.github.com/users/yuriy-kulakovskyi')
-.then((response) => {
-  return response.json();
-})
-.then((data) => {
-  let nickname = data.login;
-  let username = data.name;
-  let bio = data.bio;
-  let followers = data.followers;
-  let following = data.following;
-  let photo = data.avatar_url;
+// fetch('https://api.github.com/users/yuriy-kulakovskyi')
+// .then((response) => {
+//   return response.json();
+// })
+// .then((data) => {
+//   let nickname = data.login;
+//   let username = data.name;
+//   let bio = data.bio;
+//   let followers = data.followers;
+//   let following = data.following;
+//   let photo = data.avatar_url;
 
-  $("#github__image").css("background-image", 'url('+photo+')');
-  $(".github__link").attr("href", data.html_url);
-  $("#github__username").text(username);
-  $("#github__nickname").text(nickname);
-  $("#github__bio").text(bio);
-  $("#github__followers").text('Followers: ' + followers);
-  $("#github__following").text('Following: ' + following);
-})
+//   $("#github__image").css("background-image", 'url('+photo+')');
+//   $(".github__link").attr("href", data.html_url);
+//   $("#github__username").text(username);
+//   $("#github__nickname").text(nickname);
+//   $("#github__bio").text(bio);
+//   $("#github__followers").text('Followers: ' + followers);
+//   $("#github__following").text('Following: ' + following);
+// })
 
 fetch('https://api.github.com/users/yuriy-kulakovskyi/repos')
 .then((response) =>  {
   return response.json();
 })
 .then((data) => {
-  console.log(data);
-  $('#pelio__title').text(data[8].name);
-  $('#pelio__descr').text(data[8].description);
-  $('#pelio__link').attr('href', data[8].clone_url);
-  $('#pelio-image').attr('href', data[8].homepage);
+  function tree (repo) {
+    return repo.name === 'chrsitmas-tree-client';
+  }
 
-  $('#weather__title').text(data[20].name);
-  $('#weather__descr').text(data[20].description);
-  $('#weather__link').attr('href', data[20].clone_url);
-  $('#weather-image').attr('href', data[20].homepage);
+  function tita (repo) {
+    return repo.name === 'Tita';
+  }
 
-  $('#password__title').text(data[21].name);
-  $('#password__descr').text(data[21].description);
-  $('#password__link').attr('href', data[21].clone_url);
-  $('#password-image').attr('href', data[21].clone_url);
+  function tgForm (repo) {
+    return repo.name === 'telegram-form';
+  }
+
+  $('#pelio__title').text(data.find(tree).name);
+  $('#pelio__descr').text(data.find(tree).description);
+  $('#pelio__link').attr('href', data.find(tree).clone_url);
+  $('#pelio-image').attr('href', data.find(tree).homepage);
+
+  data.find(tree).topics.slice(0, 3).map(i => {
+    $("#pelio__topics").append(`<span>${i}</span>`);
+  })
+
+  $('#weather__title').text(data.find(tita).name);
+  $('#weather__descr').text(data.find(tita).description);
+  $('#weather__link').attr('href', data.find(tita).clone_url);
+  $('#weather-image').attr('href', data.find(tita).homepage);
+
+  data.find(tita).topics.slice(0, 5).map(i => {
+    $("#weather__topics").append(`<span>${i}</span>`);
+  })
+
+  $('#password__title').text(data.find(tgForm).name);
+  $('#password__descr').text(data.find(tgForm).description);
+  $('#password__link').attr('href', data.find(tgForm).clone_url);
+  $('#password-image').attr('href', data.find(tgForm).homepage);
+
+  data.find(tgForm).topics.slice(0, 3).map(i => {
+    $("#password__topics").append(`<span>${i}</span>`);
+  })
 })
 
 fetch('https://api.github.com/users/yuriy-kulakovskyi')
